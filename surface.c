3b0f15c98fe286b68134cb8098095d2e15c48f4e
Paul LaMendola <paulguy119@gmail.com>
2013-03-13 20:32:23 -0400

    forgot some files

---
+++ surface.c
@@ -0,0 +1,19 @@
+#include "surface.h"
+
+Surface *surface_init(int width, int height) {
+	Surface *surface;
+
+	surface = (Surface *)malloc(sizeof(Surface));
+	if(surface == NULL) {
+		return(NULL);
+	}
+	surface->width = width;
+	surface->height = height;
+	surface->luma = (char *)malloc(sizeof(char) * width * height);
+	if(surface->luma == NULL) {
+		free(surface);
+		return(NULL);
+	}
+
+	return(surface);
+}
